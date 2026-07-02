c3b5bbfb6357e3f8a70e1fd0d09a5a802f99b77f
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:16:34 -0400

    mlb API

--- mlb.py
+++ mlb.py
@@ -1,1 +1,6 @@
-# await mlb.start(self)
+import requests
+
+def get_todays_games():
+    url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1"
+    response = requests.get(url, timeout=10)
+    return response.json()
