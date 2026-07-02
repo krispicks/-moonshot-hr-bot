b0b15c6cda9934a4cd89fe52a151a5ff2736dcf9
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:42:30 -0400

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,1 +1,10 @@
+posted_home_runs = set()
 
+def already_posted(game_pk, play_id):
+    key = f"{game_pk}-{play_id}"
+
+    if key in posted_home_runs:
+        return True
+
+    posted_home_runs.add(key)
+    return False
