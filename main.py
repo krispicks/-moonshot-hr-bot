a8ff9aa2ffbc7ac61301ad69490114abef1bda34
krispicks <krisbellamy179@gmail.com>
2026-07-02 09:36:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import os
-import asyncio
 import discord
-import mlb
 import alerts
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-
-posted_home_runs = set()
 
 
 class Bot(discord.Client):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
+        ch = None
+
         if CHANNEL_ID:
             ch = self.get_channel(int(CHANNEL_ID))
+            print(f"Channel: {ch}")
+
             if ch:
                 await ch.send("✅ DingerHQ bot is online!")
 
+        print("Starting HR watcher...")
         await alerts.watch_home_runs(self, ch)
+
 
 intents = discord.Intents.default()
 
