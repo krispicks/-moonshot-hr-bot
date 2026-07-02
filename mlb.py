f53beda2bff88cd15f8a3b5365b018a72f7eec09
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:23:33 -0400

    Added live MLB API

--- mlb.py
+++ mlb.py
@@ -1,6 +1,11 @@
 import requests
 
-def get_todays_games():
-    url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1"
+def get_live_games():
+    url = "https://statsapi.mlb.com/api/v1.1/game/changes?updatedSince=1"
+    response = requests.get(url, timeout=10)
+    return response.json()
+
+def get_game_feed(game_pk):
+    url = f"https://statsapi.mlb.com/api/v1.1/game/{game_pk}/feed/live"
     response = requests.get(url, timeout=10)
     return response.json()
