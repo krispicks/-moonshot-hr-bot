a2e7fc3c32c3bcb0c4a66e359cc3d6d8451eccb3
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:43:13 -0400

    Update embeds.py

--- embeds.py
+++ embeds.py
@@ -1,1 +1,16 @@
+import discord
 
+def home_run_embed(player, team, distance, exit_velocity):
+    embed = discord.Embed(
+        title="💣 HOME RUN!",
+        color=0xff2d55
+    )
+
+    embed.add_field(name="Player", value=player, inline=False)
+    embed.add_field(name="Team", value=team, inline=True)
+    embed.add_field(name="Distance", value=f"{distance} ft", inline=True)
+    embed.add_field(name="Exit Velocity", value=f"{exit_velocity} mph", inline=True)
+
+    embed.set_footer(text="Moonshot HR Bot")
+
+    return embed
