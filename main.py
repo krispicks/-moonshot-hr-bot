0bd640596ef86c63ebb2a4622fa46377f680cdd7
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:11:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-async def on_ready(self):
-    print(f"Logged in as {self.user}")
+import os
+import discord
+import mlb
 
-    games = mlb.get_todays_games()
-    print("Connected to MLB API!")
-    print(games)
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-    if CHANNEL_ID:
-        ch = self.get_channel(int(CHANNEL_ID))
-        if ch:
-            await ch.send("✅ DingerHQ bot is online.")
 
-    await mlb.start(self)
+class Bot(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+
+        games = mlb.get_todays_games()
+        print("Connected to MLB API!")
+
+        dates = games.get("dates", [])
+
+        if dates:
+            print(f"Today's games: {len(dates[0]['games'])}")
+        else:
+            print("No games today.")
+
+        if CHANNEL_ID:
+            ch = self.get_channel(int(CHANNEL_ID))
+            if ch:
+                await ch.send("✅ DingerHQ bot is online.")
+
+
+intents = discord.Intents.default()
+
+client = Bot(intents=intents)
+
+client.run(TOKEN)
