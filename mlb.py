f5312365f9b1c59d0af200e43363318f6881039b
krispicks <krisbellamy179@gmail.com>
2026-07-02 17:56:11 -0400

    Update mlb.py

--- mlb.py
+++ mlb.py
@@ -47,7 +47,7 @@ def get_home_run_events(game_pk):
             continue
 
         hit = play.get("hitData", {})
-
+print("HIT DATA:", hit, flush=True)
         if play["about"]["halfInning"].lower() == "top":
             team = (
                 feed.get("gameData", {})
