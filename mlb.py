dcb85558e90baa4d33121445f821c054fa1e79da
krispicks <krisbellamy179@gmail.com>
2026-07-02 05:04:50 -0400

    Remove get_todays_games function and import

--- mlb.py
+++ mlb.py
@@ -1,6 +1,1 @@
-import requests
-
-def get_todays_games():
-    url = "https://statsapi.mlb.com/api/v1/schedule?sportId=1"
-    response = requests.get(url, timeout=10)
-    return response.json()
+# await mlb.start(self)
